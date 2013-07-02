387fa2908e84aba9513836dac7030a278a7ba7b3
Boaz Stolk <boaz.stolk@gmail.com>
2013-07-03 00:45:34 +0200

    Put all code in a object, added check for local file or http/www file, added processmessage Message widget function, updated comments and print messages, also load before/when handling write

--- app.py
+++ app.py
@@ -27,149 +27,136 @@ try:
 except ImportError:
   from tkFileDialog import askdirectory
 
-unzipapp = tk.Tk()
-unzipapp.title("unzipapp")
+class CUnzipApp:
 
+  def __init__(self, unzipapp):
+    self.window = tk.Frame(unzipapp)
+    self.inputframe = tk.Frame(self.window, width = 200, height = 100)
+    self.outputframe = tk.Frame(self.window, width = 200, height = 100)
+    self.messageframe = tk.Frame(self.window, width = 200, height = 20)
 
-#def callback():
-#    print "click!"
-#    print var.get()
+    self.sourcetext = tk.Label(self.inputframe, text='input file: ')
+    self.sourcepath = tk.Entry(self.inputframe)
+    self.browsesourcefile = tk.Button(self.inputframe, text = 'browse...', command=self.setsourcefile)
+    self.loadfile = tk.Button(self.inputframe, text = 'load file', command=self.loadsourcefile)
 
-#var = tk.IntVar()
-#c = tk.Checkbutton(unzipapp, text = "Expand", variable = var)
-#c.pack()
+    self.destinationtext = tk.Label(self.outputframe, text='output path: ')
+    self.destinationpath = tk.Entry(self.outputframe)
 
-# class CUnzipApp:
+    self.overwrite = tk.IntVar()
+    self.overwritecontent = tk.Checkbutton(self.outputframe, text = 'overwrite', variable=self.overwrite, onvalue=True, offvalue=False)
 
-#     def __init__(self, master):
+    self.browsedestinationfile = tk.Button(self.outputframe, text = 'browse...', command=self.setdestinationpath)
+    self.writecontent = tk.Button(self.outputframe, text = 'write!', command=self.extracttopath)
 
-#         mainframe = tk.Frame(master)
-#         mainframe.pack()
+    self.messages = tk.StringVar()
+    self.processmessages = tk.Message(self.messageframe, anchor='nw', justify='left', width=500, textvariable=self.messages)
 
-#         self.button = tk.Button(mainframe, text="QUIT", fg="red", command=mainframe.quit)
-#         self.button.pack(side='left')
+    self.window.grid(column=0, row=0)
+    self.inputframe.grid(column=0, row=0)
+    self.outputframe.grid(column=0, row=1)
+    self.messageframe.grid(row=2)
 
-#         self.hi_there = tk.Button(mainframe, text="Hello", command=self.say_hi)
-#         self.hi_there.pack(side='left')
+    self.sourcetext.grid(column=0, row=0)
+    self.sourcepath.grid(column=1, row=0)
+    self.browsesourcefile.grid(column=3, row=0)
+    self.loadfile.grid(column=4, row=0)
 
-#     def say_hi(self):
-#         print "hi there, everyone!"
+    self.destinationtext.grid(column=0, row=1)
+    self.destinationpath.grid(column=1, row=1)
+    self.overwritecontent.grid(column=2, row=1)
+    self.browsedestinationfile.grid(column=3, row=1)
+    self.writecontent.grid(column=4, row=1)
 
-# root = tk.Tk()
+    self.processmessages.grid(row=2)
 
-# app = CUnzipApp(root)
+    tkMessageBox.showwarning(message="WARNING!\n\nThis app can erase all contents of directories with the 'overwrite' checkbox being marked! Do not use it or use with caution!")
 
-# root.mainloop()
-# root.destroy() # optional; see description below
+  def setsourcefile(self):
+    #print("setsourcefile()")
+    filepath = askopenfilename(filetypes = [('ZIP-files', '.zip'), ('All files', '.*')], title = "Pick a ZIP-file")
+    print(filepath)
+    self.sourcepath.delete(0, last='end')
+    self.sourcepath.insert(0, filepath)
 
-def setsourcefile():
-  #print("setsourcefile()")
-  filepath = askopenfilename(filetypes = [('ZIP-files', '.zip'), ('All files', '.*')], title = "Pick a ZIP-file")
-  print(filepath)
-  sourcepath.delete(0, last='end')
-  sourcepath.insert(0, filepath)
+  def loadsourcefile(self):
+    #print("loadsourcefile()")
+    print("Load from:", self.sourcepath.get())
+    self.processmessage("Load from:" + str(self.sourcepath.get()))
+    # not so nice check for if it is probably a url or local file...
+    if (self.sourcepath.get().startswith("http") or self.sourcepath.get().startswith("www")):
+      sourcefilepath = self.sourcepath.get()
+    else:
+      sourcefilepath = "file:" + self.sourcepath.get()
+    (source, headers) = urllib.urlretrieve(sourcefilepath, )
+    if ( zipfile.is_zipfile(source) == True ):
+      print("File is valid ZIP file")
+      self.processmessage("File is valid ZIP file")
+      self.sourcepath.delete(0, last='end')
+      self.sourcepath.insert(0, source)
+      print("New source file", source)
+      self.processmessage("New source file" + str(source))
+    else:
+      print("ERROR: File is NOT a valid ZIP file")
+      self.processmessage("ERROR: File is NOT a valid ZIP file")
 
-def loadsourcefile():
-  #print("loadsourcefile()")
-  print("Load from:", sourcepath.get())
-  #urllib.urlcleanup() # clear cache
-  (source, headers) = urllib.urlretrieve(sourcepath.get(), )
-  if ( zipfile.is_zipfile(source) == True ):
-    print("file is valid ZIP file")
-    sourcepath.delete(0, last='end')
-    sourcepath.insert(0, source)
-    print("New source file", source)
-  else:
-    print("ERROR: File is NOT a valid ZIP file")
+  def setdestinationpath(self):
+    #print("setdestinationpath()")
+    path = askdirectory(initialdir="/", title="Select a drive or folder")
+    print(path)
+    self.destinationpath.delete(0, last='end')
+    self.destinationpath.insert(0, path)
 
-def setdestinationpath():
-  #print("setdestinationpath()")
-  path = askdirectory(initialdir="/", title="Select a drive or folder")
-  print(path)
-  destinationpath.delete(0, last='end')
-  destinationpath.insert(0, path)
+  def erasepathcontent(self):
+    #print("erasepathcontent()")
+    for files in os.listdir(self.destinationpath.get()):
+      path = os.path.join(self.destinationpath.get(), files)
+      try:
+        if os.path.isfile(path):
+          print("Deleted entry:", path)
+          os.unlink(path)
+        else:
+          shutil.rmtree(path)
+          print("Deleted entry:", path)
+      except Exception as e:
+        print("Exception caught:", e)
 
-def erasepathcontent():
-  #print("erasepathcontent()")
-  for files in os.listdir(destinationpath.get()):
-    path = os.path.join(destinationpath.get(), files)
-    try:
-      if os.path.isfile(path):
-        print("Deleted entry:", path)
-        os.unlink(path)
-      else:
-        shutil.rmtree(path)
-        print("Deleted entry:", path)
-    except Exception as e:
-      print("Exception caught:", e)
-
-def extracttopath():
-  #print("extracttopath()")
-  if (sourcepath.get() != ""):
-    if (os.path.isdir(destinationpath.get())):
-      if (overwrite.get() == True):
-        question = "Are you sure you want to ! ERASE ! contents of " + str(destinationpath.get()) + " ?"
-        print("WARNING: no check for illegal content of zip-file!")
-        areyousure = tkMessageBox.askyesno(message=question, icon='question', title='ERASE ALL!?')
-        if (areyousure == True):
-          print("Overwrite enabled! Erasing contents of:", destinationpath.get())
-          erasepathcontent()
+  def extracttopath(self):
+    #print("extracttopath()")
+    if (self.sourcepath.get() != ""):
+      self.loadsourcefile()
+      if (os.path.isdir(self.destinationpath.get())):
+        if (self.overwrite.get() == True):
+          question = "Are you sure you want to ! ERASE ! contents of " + str(self.destinationpath.get()) + " ?"
+          print("WARNING: no check for illegal content of zip-file!")
+          self.processmessage("WARNING: no check for illegal content of zip-file!")
+          areyousure = tkMessageBox.askyesno(message=question, icon='question', title='ERASE ALL!?')
+          if (areyousure == True):
+            print("Overwrite enabled! Erasing contents of:", self.destinationpath.get())
+            self.processmessage("Overwrite enabled! Erasing contents of:" + str(self.destinationpath.get()))
+            self.erasepathcontent()
+            print("Unpacking files...")
+            self.processmessage("Unpacking files...")
+            myfile = ZipFile(self.sourcepath.get())
+            myfile.extractall(self.destinationpath.get())
+            print("Done!")
+            self.processmessage("Done!")
+        else:
+          print("Overwrite disabled! Appending to:", self.destinationpath.get())
           print("Unpacking files...")
-          myfile = ZipFile(sourcepath.get())
-          myfile.extractall(destinationpath.get())
+          myfile = ZipFile(self.sourcepath.get())
+          myfile.extractall(self.destinationpath.get())
           print("Done!")
       else:
-        print("Overwrite disabled! Appending to:", destinationpath.get())
-        print("Unpacking files...")
-        myfile = ZipFile(sourcepath.get())
-        myfile.extractall(destinationpath.get())
-        print("Done!")
+        print("Destinationpath is not a valid directory")
     else:
-      print("destinationpath is not a directory")
-  else:
-    print("sourcepath is empty")
+      print("Sourcepath is empty")
+      self.processmessage("Sourcepath is empty!")
 
-print("App is running")
+  def processmessage(self, message):
+    self.messages.set(message)
 
-tkMessageBox.showwarning(message="WARNING!\n\nThis app can erase all contents of directories when the 'overwrite' checkbox being marked! Do not use it or use with caution!")
-
-window = tk.Frame(unzipapp)
-inputframe = tk.Frame(window, width = 200, height = 100)
-outputframe = tk.Frame(window, width = 200, height = 100)
-messageframe = tk.Frame(window, width = 200, height = 20)
-
-sourcetext = tk.Label(inputframe, text='input file: ')
-sourcepath = tk.Entry(inputframe)
-browsesourcefile = tk.Button(inputframe, text = 'browse...', command=setsourcefile)
-loadfile = tk.Button(inputframe, text = 'load file', command=loadsourcefile)
-
-destinationtext = tk.Label(outputframe, text='output path: ')
-destinationpath = tk.Entry(outputframe)
-
-overwrite = tk.IntVar()
-overwritecontent = tk.Checkbutton(outputframe, text = 'overwrite', variable=overwrite, onvalue=True, offvalue=False)
-
-browsedestinationfile = tk.Button(outputframe, text = 'browse...', command=setdestinationpath)
-writecontent = tk.Button(outputframe, text = 'write!', command=extracttopath)
-
-processmessages = tk.Message(messageframe, text = 'important message!')
-
-window.grid(column=0, row=0)
-inputframe.grid(column=0, row=0)
-outputframe.grid(column=0, row=1)
-messageframe.grid(column=0, row=2)
-
-sourcetext.grid(column=0, row=0)
-sourcepath.grid(column=1, row=0)
-browsesourcefile.grid(column=3, row=0)
-loadfile.grid(column=4, row=0)
-
-destinationtext.grid(column=0, row=1)
-destinationpath.grid(column=1, row=1)
-overwritecontent.grid(column=2, row=1)
-browsedestinationfile.grid(column=3, row=1)
-writecontent.grid(column=4, row=1)
-
-processmessages.grid(column=0, row=2)
-
-unzipapp.mainloop()
+root = tk.Tk()
+root.title("Unzipapp")
+CUnzipApp(root)
+root.mainloop()
